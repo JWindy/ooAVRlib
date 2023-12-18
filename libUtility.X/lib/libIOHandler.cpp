8be7f4b214b1010b658e28f43e26aaaa6e3a71e0
JWindy <j.windy@gmx.de>
2023-12-18 18:19:18 +0100

    Change Ctor to explicit

--- libUtility.X/lib/libIOHandler.cpp
+++ libUtility.X/lib/libIOHandler.cpp
@@ -3,50 +3,49 @@
 
 #include "libIOHandler.h"
 
-libIOHandler::libIOHandler(void){
+libIOHandler::libIOHandler(void) {
     version.major = 0;
     version.minor = 1;
 }
 
-ver_t libIOHandler::getVersion(void){
+ver_t libIOHandler::getVersion(void) {
     return version;
 }
 
-void libIOHandler::setPinOutput(uint8_t argPin){
-    uint8_t ddrPin = getDdr(argPin);   
+void libIOHandler::setPinOutput(uint8_t argPin) {
+    uint8_t ddrPin = getDdr(argPin);
     DDRB |= (1 << ddrPin);
     setPinLow(argPin);
 }
-      
-void libIOHandler::setPinHigh(uint8_t argPin){
-    PORTB |= (1 << argPin); 
+
+void libIOHandler::setPinHigh(uint8_t argPin) {
+    PORTB |= (1 << argPin);
 }
 
-void libIOHandler::setPinLow(uint8_t argPin){
-    PORTB &= ~(1 << argPin); 
+void libIOHandler::setPinLow(uint8_t argPin) {
+    PORTB &= ~(1 << argPin);
 }
 
-void libIOHandler::togglePin(uint8_t argPin){
-    PORTB ^= (1 << argPin); 
+void libIOHandler::togglePin(uint8_t argPin) {
+    PORTB ^= (1 << argPin);
 }
 
-void libIOHandler::setPinInput(uint8_t argPin, uint8_t argPullUp){
+void libIOHandler::setPinInput(uint8_t argPin, uint8_t argPullUp) {
     uint8_t ddrPin = getDdr(argPin);
-    
+
     DDRB &= ~(1 << ddrPin);
-    
-    if(argPullUp){
+
+    if (argPullUp) {
         PORTB |= (1 << argPin);
-    }
-    else{
+    } else {
         PORTB &= ~(1 << argPin);
     }
 }
 
-uint8_t libIOHandler::readPinRaw(uint8_t argPin){
+uint8_t libIOHandler::readPinRaw(uint8_t argPin) {
     uint8_t pinState = PINB & (1 << argPin);
     return pinState;
-}   
+}
 
 /*
 void configureInterrupt(uint8_t argPin, uint8_t argTriggerType, 
@@ -67,30 +66,30 @@ void disableInterruptOnPin(uint8_t argPin){
     
 } */
 
-uint8_t libIOHandler::getDdr(uint8_t argPin){
+uint8_t libIOHandler::getDdr(uint8_t argPin) {
     uint8_t ddrPin = 0;
-    
-    #ifdef __AVR_ATtiny85__
-        switch (argPin){
-            case PB0:
-                ddrPin    = DDB0;
-                break;
-            case PB1:
-                ddrPin    = DDB1;
-                break;
-            case PB2:
-                ddrPin    = DDB2;            
-                break;
-            case PB3:
-                ddrPin    = DDB3;             
-                break;
-            case PB4:
-                ddrPin    = DDB4; 
-                break;
-        }    
-    #elif
-        #error "libIOHandler not implemented for selected MUC."
-    #endif
+
+#ifdef __AVR_ATtiny85__
+    switch (argPin) {
+        case PB0:
+            ddrPin = DDB0;
+            break;
+        case PB1:
+            ddrPin = DDB1;
+            break;
+        case PB2:
+            ddrPin = DDB2;
+            break;
+        case PB3:
+            ddrPin = DDB3;
+            break;
+        case PB4:
+            ddrPin = DDB4;
+            break;
+    }
+#else
+#    error "libIOHandler not implemented for selected MUC."
+#endif
 
     return ddrPin;
 }
--- libUtility.X/libUtility.h
+++ libUtility.X/libUtility.h
@@ -26,43 +26,46 @@ Copyright:      see LICENSE_software.md in git hub root folder
 
 #include <avr/io.h>
 
-struct ver_t{
+struct ver_t {
     uint8_t major;
     uint8_t minor;
 };
 
-enum status_t{
-    INIT_STATE,     //initialisation not yet completed
-    READY_STATE,    //initialisation completed
+enum status_t {
+    INIT_STATE, //initialisation not yet completed
+    READY_STATE, //initialisation completed
     IDLE_STATE,
     BUSSY_STATE,
     ERROR_STATE
 };
 
-class Semaphore{
+class Semaphore {
     //provides basic operations to lock a ressource/object to  prevent access by 
     //to many users. For argS == 1 -> mutex -> only one user can access the 
     //ressouce/object
-    public:
-        //ArgMaxNumberOfUser defines the number of users, which can access the protected resource/class
-                        Semaphore(uint8_t argMaxNumberOfUser);
-        //If the resourece is available upon request, lock() returns a key, which is 
-        //required to unlock the resource -> argKey. 
-        //locking requires 3 bytes of program memory
-        uint8_t         lock(void);
-        //If the ressource can be unlocked (ressource locked and key correct), 
-        //unlock returns 0, otherwise it returns argKey
-        //unlocking requires 10 byte of program memory
-        uint8_t         unlock(uint8_t argKey);
-        //Any critical operation of the ressource/object can be guarded by 
-        //checking the key of the user -> returns 1, if the key is correct, 
-        //otherwise returns 0
-        //checking the key requires to 2 byte of program memory
-        uint8_t         checkKey(uint8_t argKey);
-        
-    private:
-        uint8_t         userCount;
-        uint8_t         maxNumberOfUser;
+public:
+    //ArgMaxNumberOfUser defines the number of users, which can access the protected resource/class
+    explicit Semaphore(uint8_t argMaxNumberOfUser);
+    
+    //If the resourece is available upon request, lock() returns a key, which is 
+    //required to unlock the resource -> argKey. 
+    //locking requires 3 bytes of program memory
+    uint8_t lock(void);
+    
+    //If the ressource can be unlocked (ressource locked and key correct), 
+    //unlock returns 0, otherwise it returns argKey
+    //unlocking requires 10 byte of program memory
+    uint8_t unlock(uint8_t argKey);
+    
+    //Any critical operation of the ressource/object can be guarded by 
+    //checking the key of the user -> returns 1, if the key is correct, 
+    //otherwise returns 0
+    //checking the key requires to 2 byte of program memory
+    uint8_t checkKey(uint8_t argKey);
+
+private:
+    uint8_t userCount;
+    uint8_t maxNumberOfUser;
 };
 #endif	/* LIBUTILITY_H */
 
